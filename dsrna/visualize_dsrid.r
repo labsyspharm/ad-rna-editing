a4ba07f958eb76b9da3c0cf866bba84c231e6a52
Clemens Hug <clemens.hug@gmail.com>
2025-03-20 20:20:24 -0400

    Rework analysis of edits in REs, check overall editing outside of REs, in MT genes

--- dsrna/visualize_dsrid.r
+++ dsrna/visualize_dsrid.r
@@ -1,2 +1,15 @@
-library(tidyverser)
+library(tidyverse)
 
+
+x <- read_tsv("~/Downloads/dsRID_prediction_randomf.tsv")
+
+
+x_short <- x %>%
+  select(
+    std_start, std_end,
+    mean_start, mean_end,
+    len_skip, coverage, num_skip,
+    skip_ratio,
+    chr, start, end,
+    pred_1
+  )
